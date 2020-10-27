abab493a5071c6527eeea467529cb86cdec16601
Ravi Itha <itharav@amazon.com>
2020-10-26 21:57:05 -0500

    Initial code commit

--- README.md
+++ README.md
@@ -1,17 +1,86 @@
-## My Project
+# Amazon Kinesis Data Analytics Flink Snapshot Manager
 
-TODO: Fill this README out!
+[Amazon Kinesis Data Analytics Flink](https://docs.aws.amazon.com/kinesisanalytics/latest/java/how-it-works.html) Snapshot Manager offers the following benefits:
 
-Be sure to:
+   1. takes a new snapshot
+   1. checks if an application has more older snapshots than required
+   1. deletes older snapshots and retains most recent
 
-* Change the title in this README
-* Edit your repository description on GitHub
+This will be deployed as a [AWS Lambda](https://aws.amazon.com/lambda/) function and scheduled using [Amazon CloudWatch Events](https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/WhatIsCloudWatchEvents.html) e.g. once in a day or week.
 
-## Security
+---
 
-See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more information.
+## Architecture
 
-## License
+![Alt](./kda_flink_snapshot_manager.png)
 
-This library is licensed under the MIT-0 License. See the LICENSE file.
+---
 
+## Process Flow Diagram
+
+![Alt](./kda_flink_snapshot_manager_process_flow_diagram.png)
+
+---
+
+## Pre-requisites
+
+  1. Python 3.7
+  1. IDE e.g. [PyCharm](https://www.jetbrains.com/pycharm/)
+  1. Access to AWS Account
+  1. A running Kinesis Data Analytics Flink Application
+
+---
+
+## AWS Service Requirements
+
+The following AWS services are required to deploy this starter kit:
+
+ 1. 1 AWS Lambda Function
+ 1. 1 Amazon SNS Topic
+ 1. 1 Amazon DynamoDB Table
+ 1. 1 IAM role with 4 policies
+ 1. 1 AWS CloudWatch Event Rule
+
+---
+
+## Deployment Instructions using AWS Console
+
+1. Create an SNS Topic and subscriber the required e-mail id(s)
+1. Create a DynamoDB Table
+   1. Table name= ```flink_snapshot_manager_status```
+   1. Primary partition key: name= ```flink_app_name```, type= String
+   1. Primary sort key: name= ```snapshot_manager_run_id```, type= Number
+   1. Provisioned read capacity units = 5
+   1. Provisioned write capacity units = 5
+1. Create following IAM policies
+   1. IAM policy with name ```iam_policy_dynamodb``` using [this sample](./resources/iam_policy_dynamodb.json)
+   1. IAM policy with name ```iam_policy_sns``` using [this sample](./resources/iam_policy_sns.json)
+   1. IAM policy with name ```iam_policy_kinesisanalytics``` using [this sample](./resources/iam_policy_kinesisanalytics.json)
+   1. IAM policy with name ```iam_policy_cloudwatch_logs``` using [this sample](./resources/iam_policy_cloudwatch_logs.json)
+1. Create an IAM role with name ```kda_flink_snapshot_manager_iam_role``` and attach above policies
+1. Deploy **kda_flink_session_manager** function
+
+    1. Runtime = Python 3.7
+    1. IAM role = kda_flink_snapshot_manager_iam_role
+    1. Function code = Copy the contents from [amazon_kd_flink_snapshot_manager.py](./amazon_kd_flink_snapshot_manager.py)
+    1. Under basic settings:
+        1. Timeout = e.g. 5 minutes
+        1. Memory = e.g. 128 MB
+    1. Environment variable = as defined in the following table
+    
+         | Key   | Value  | Description |
+         |-------| -------| ----------- |
+         | region  | us-east-1 | AWS region |
+         | kda_flink_app_name | ```Application Name``` | Name of the Kinesis Data Analytics Flink Application |
+         | flink_snapshot_manager_table  | ```flink_snapshot_manager_status``` | Name of the DynamoDB table used to track the status |
+         | primary_partition_key_name | ```flink_app_name``` | Primary partition key name |
+         | primary_sort_key_name | ```snapshot_manager_run_id``` | Primary sort key name |
+         | sns_topic_arn | ```SNS Topic ARN``` | SNS Topic ARN  |
+         | number_of_older_snapshots_to_retain | ```30``` | The number of most recent snapshots to be retained  |
+         | snapshot_creation_wait_time_seconds | ```15``` | Time gap in seconds between consecutive checks to get the status of snapshot creation  |
+
+---
+
+## License Summary
+
+This sample code is made available under the MIT license. See the LICENSE file.
---
+++ amazon_kd_flink_snapshot_manager.py
@@ -0,0 +1,366 @@
+# Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
+import os
+import time
+import json
+import boto3
+import logging
+import datetime
+import botocore
+
+# setup logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def lambda_handler(event, context):
+    """
+    AWS Lambda function's entry point. It takes a snapshot of a KDA Flink application, retains the most recent X
+    snapshots, and deletes the rest. For X, see parameter 'num_of_older_snapshots_to_retain'.
+    :param event:
+    :param context:
+    :return:
+    """
+    print('Event received.', json.dumps(event, indent=4))
+    # read environment variables
+    region = os.environ['aws_region']
+    flink_app_name = os.environ['kda_flink_app_name']
+    ddb_table_name = os.environ['flink_app_snapshot_manager_table']
+    primary_partition_key_name = os.environ['primary_partition_key_name']
+    primary_sort_key_name = os.environ['primary_sort_key_name']
+    sns_topic_arn = os.environ['sns_topic_arn']
+    num_of_older_snapshots_to_retain = int(os.environ['number_of_older_snapshots_to_retain'])
+    snapshot_creation_wait_time_seconds = int(os.environ['snapshot_creation_wait_time_seconds'])
+
+    # setup clients
+    sns = boto3.client('sns', region)
+    dynamodb = boto3.client('dynamodb', region)
+    kinesis_analytics = boto3.client('kinesisanalyticsv2', region)
+
+    # initialize variables
+    return_response = {}
+    sorted_snapshots = []
+    deleted_snapshots = []
+    not_deleted_snapshots = []
+    snapshot_deletion_status = {}
+    snapshot_completed = False
+    snapshot_manager_run_id = int(round(time.time() * 1000))
+    snapshot_name = 'custom_' + str(snapshot_manager_run_id)
+
+    # describe application to get application status and current version
+    response = describe_flink_application(kinesis_analytics, flink_app_name)
+
+    if response['ApplicationDetail']['ApplicationStatus'] == 'RUNNING':
+        snapshot_creation_res = take_app_snapshot(kinesis_analytics, flink_app_name, snapshot_name)
+        snapshot_creation_res['app_version'] = response['ApplicationDetail']['ApplicationVersionId']
+        print("Snapshot creation initiated: {0}, name: {1}.".format(snapshot_creation_res['is_initiated'],
+                                                                    snapshot_creation_res['snapshot_name']))
+
+        # If the snapshot creation is initiated then check if it is completed
+        if snapshot_creation_res['is_initiated']:
+            return_response = {'statusCode': 200, 'body': json.dumps('Snapshot creation request was successful!')}
+            max_checks = 4
+            checks_done = 0
+            while checks_done < max_checks:
+                time.sleep(snapshot_creation_wait_time_seconds)
+                snapshots = list_flink_app_snapshots(kinesis_analytics, flink_app_name,
+                                                     response['ApplicationDetail']['ApplicationVersionId'])
+                print('Application {0} of version {1} has {2} snapshots: ', flink_app_name,
+                      response['ApplicationDetail']['ApplicationVersionId'], len(snapshots))
+                sorted_snapshots = sorted(snapshots, key=lambda k: k['SnapshotCreationTimestamp'], reverse=True)
+                latest_snapshot = sorted_snapshots[0]
+                if latest_snapshot['SnapshotName'] == snapshot_creation_res['snapshot_name']:
+                    if latest_snapshot['SnapshotStatus'] == 'READY':
+                        snapshot_completed = True
+                        checks_done = 4
+                        print('Snapshot crated successfully. Snapshot: {0}'.format(latest_snapshot))
+                    else:
+                        checks_done += 1
+                else:
+                    print('No checkpoint found with the name: {0}'.format(snapshot_creation_res['snapshot_name']))
+
+        # check if the number of historical snapshots exceeds the threshold. If yes, delete them one by one
+        if len(sorted_snapshots) > num_of_older_snapshots_to_retain:
+            snapshots_to_be_deleted = sorted_snapshots[num_of_older_snapshots_to_retain:-1]
+            for snapshot_to_be_deleted in snapshots_to_be_deleted:
+                snapshot_deleted = delete_snapshot(kinesis_analytics, flink_app_name, snapshot_to_be_deleted)
+                if snapshot_deleted:
+                    deleted_snapshots.append(snapshot_to_be_deleted)
+                    print(
+                        'Snapshot deleted: {0}, name: {1}'.format(snapshot_deleted,
+                                                                  snapshot_to_be_deleted['SnapshotName']))
+                else:
+                    not_deleted_snapshots.append(snapshot_to_be_deleted)
+
+            # add deleted and not-deleted snapshots to snapshot_deletion_status dictionary
+            snapshot_deletion_status['deleted_snapshots'] = deleted_snapshots
+            snapshot_deletion_status['not_deleted_snapshots'] = not_deleted_snapshots
+        else:
+            logger.info('Number of historical snapshots less than the threshold. No need to delete any snapshots.')
+
+        # insert Snapshot manager status to DynamoDB
+        if snapshot_completed or len(snapshot_deletion_status['deleted_snapshots']) > 0 or len(
+                snapshot_deletion_status['not_deleted_snapshots']) > 0:
+            track_snapshot_manager_status(dynamodb, ddb_table_name, primary_partition_key_name, primary_sort_key_name,
+                                          flink_app_name, snapshot_manager_run_id, latest_snapshot,
+                                          snapshot_deletion_status)
+
+        # Send SNS notification
+        if snapshot_completed:
+            send_sns_notification(sns, sns_topic_arn, flink_app_name, snapshot_manager_run_id, snapshot_name,
+                                  latest_snapshot, True)
+        else:
+            send_sns_notification(sns, sns_topic_arn, flink_app_name, snapshot_manager_run_id,
+                                  snapshot_name, None, False)
+    else:
+        print('Flink application {0} is not running. A new snapshot cannot be taken.')
+        return_response = {'statusCode': 300, 'body': json.dumps('Snapshot creation request was not successful!')}
+        notify_when_app_is_not_running(sns, sns_topic_arn, flink_app_name, snapshot_manager_run_id)
+
+    return return_response
+
+
+def describe_flink_application(kin_analytics, flink_app_name):
+    """
+    This function describes a KDA Flink Application
+    :param kin_analytics:
+    :param flink_app_name:
+    :return:
+    """
+    try:
+        res = kin_analytics.describe_application(ApplicationName=flink_app_name, IncludeAdditionalDetails=True)
+    except botocore.exceptions.ClientError as error:
+        if error.response['Error']['Code'] == 'ResourceNotFoundException':
+            logger.warning('The requested Kinesis Data Analytics Flink Application was not found')
+        else:
+            print('Error Message: {}'.format(error.response['Error']['Message']))
+    return res
+
+
+def list_flink_app_snapshots(kin_analytics, flink_app_name, app_ver_id):
+    """
+    This function get a list of snapshots for a KDA Flink Application
+    :param kin_analytics:
+    :param flink_app_name:
+    :param app_ver_id:
+    :return:
+    """
+    app_snapshots_latest_version = []
+    try:
+        response = kin_analytics.list_application_snapshots(ApplicationName=flink_app_name, Limit=10)
+        snapshot_summary_list = response['SnapshotSummaries']
+        for snapshot_summary in snapshot_summary_list:
+            if app_ver_id == snapshot_summary['ApplicationVersionId']:
+                app_snapshots_latest_version.append(snapshot_summary)
+        # process next set list of items if 'NextToken' exist in the response
+        while 'NextToken' in response:
+            response = kin_analytics.list_application_snapshots(
+                ApplicationName=flink_app_name, Limit=50, NextToken=response['NextToken']
+            )
+            snapshot_summary_list = response['SnapshotSummaries']
+            for snapshot_summary in snapshot_summary_list:
+                if app_ver_id == snapshot_summary['ApplicationVersionId']:
+                    app_snapshots_latest_version.append(snapshot_summary)
+    except botocore.exceptions.ClientError as error:
+        if error.response['Error']['Code'] == 'ResourceNotFoundException':
+            logger.warning('The requested Kinesis Data Analytics Flink Application was not found')
+        else:
+            print('Error Message: {}'.format(error.response['Error']['Message']))
+
+    return app_snapshots_latest_version
+
+
+def take_app_snapshot(kin_analytics, flink_app_name, snapshot_name):
+    """
+    This function takes a Flink snapshot
+    :param kin_analytics:
+    :param flink_app_name:
+    :param snapshot_name:
+    :return:
+    """
+    snapshot_creation_resp = {
+        "app_name": flink_app_name,
+        "snapshot_name": "",
+        "is_initiated": False,
+        "error_message": "",
+        "app_version": ""
+    }
+    try:
+        res = kin_analytics.create_application_snapshot(ApplicationName=flink_app_name, SnapshotName=snapshot_name)
+        if res['ResponseMetadata']['HTTPStatusCode'] == 200:
+            snapshot_creation_resp['is_initiated'] = True
+            snapshot_creation_resp['snapshot_name'] = snapshot_name
+            logger.info('Snapshot creation initiated.')
+    except botocore.exceptions.ClientError as error:
+        snapshot_creation_resp['error_message'] = error.response['Error']['Message']
+        if error.response['Error']['Code'] == 'ResourceNotFoundException':
+            logger.warning('The requested Kinesis Data Analytics Flink Application was not found')
+        elif error.response['Error']['Code'] == 'InvalidRequestException':
+            print('Error Message: {}'.format(error.response['Error']['Message']))
+        else:
+            print('Error Message: {}'.format(error.response['Error']['Message']))
+    return snapshot_creation_resp
+
+
+def delete_snapshot(kin_analytics, flink_app_name, snapshot):
+    """
+    This function deletes a Flink snapshot
+    :param kin_analytics:
+    :param flink_app_name:
+    :param snapshot:
+    :return:
+    """
+    is_snapshot_deleted = False
+    try:
+        res = kin_analytics.delete_application_snapshot(
+            ApplicationName=flink_app_name,
+            SnapshotName=snapshot['SnapshotName'],
+            SnapshotCreationTimestamp=snapshot['SnapshotCreationTimestamp']
+        )
+        if res['ResponseMetadata']['HTTPStatusCode'] == 200:
+            is_snapshot_deleted = True
+    except botocore.exceptions.ClientError as error:
+        if error.response['Error']['Code'] == 'ResourceNotFoundException':
+            logger.warning('The requested Kinesis Data Analytics Flink Application was not found')
+        else:
+            print('Error Message: {}'.format(error.response['Error']['Message']))
+    return is_snapshot_deleted
+
+
+def notify_when_app_is_not_running(sns, topic_arn, flink_app_name, snapshot_manager_run_id):
+    """
+    This function sends a notification to Amazon SNS Topic
+    :param sns:
+    :param topic_arn:
+    :param flink_app_name:
+    :param snapshot_manager_run_id:
+    :return:
+    """
+    message = """
+                Application Team:
+
+                Snapshot Manager execution completed. Run Id: {0}. However, a new snapshot has not been taken.
+                The application {1} is not running.
+                """.format(snapshot_manager_run_id, flink_app_name)
+    try:
+        pub_response = sns.publish(TopicArn=topic_arn, Message=message,
+                                   Subject='KDA Flink Snapshot Manager Alert')
+        if pub_response['ResponseMetadata']['HTTPStatusCode'] == 200:
+            message_sent = True
+            logger.info(
+                'Message published to SNS Topic successfully. Message Id: {0}'.format(pub_response['MessageId']))
+    except botocore.exceptions.ClientError as error:
+        if error.response['Error']['Code'] == 'ResourceNotFoundException':
+            logger.warning('The requested SNS Topic was not found')
+        else:
+            print('Error Message: {}'.format(error.response['Error']['Message']))
+    return message_sent
+
+
+def send_sns_notification(sns, topic_arn, flink_app_name, snapshot_manager_run_id, snapshot_name, new_snapshot: None,
+                          snapshot_created):
+    """
+    This function sends a notification to Amazon SNS Topic
+    :param snapshot_name:
+    :param sns:
+    :param topic_arn:
+    :param flink_app_name:
+    :param snapshot_manager_run_id:
+    :param new_snapshot:
+    :param snapshot_created:
+    :return:
+    """
+    message_sent = False
+    if snapshot_created:
+        message = """
+        Application Team:
+        
+        Snapshot Manager execution completed. Run Id: {0}.
+        
+        ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        
+        New snapshot creation details:   
+             - Application Name: {1}
+             - Snapshot name: {2}
+             - Application version Id: {3}
+             - Snapshot Creation Time: {4}
+         
+        Historical snapshot(s) deletion status:         
+             - Refer DynamoDB audit table for details. primary partition key: {5}, primary sort key: {6}.
+        
+        ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    """.format(snapshot_manager_run_id, flink_app_name, new_snapshot['SnapshotName'],
+               new_snapshot['ApplicationVersionId'],
+               new_snapshot['SnapshotCreationTimestamp'], flink_app_name, snapshot_manager_run_id)
+    else:
+        message = """
+                Application Team:
+        
+                Snapshot Manager execution completed. Run Id: {0}. However, the snapshot creation process either 
+                not completed on time or failed. Please investigate Cloudwatch logs and check the Snapshots 
+                section under Amazon Kinesis - Analytics applications - Flink Application of your AWS environment. 
+                Below are the details:
+                
+                ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                
+                    - Application Name: {1}
+                    - Snapshot Name: {2}
+                    - Snapshot creation attempted at: {3}
+                    
+                ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+            """.format(snapshot_manager_run_id, flink_app_name, snapshot_name, datetime.datetime.now())
+    try:
+        pub_response = sns.publish(TopicArn=topic_arn, Message=message,
+                                   Subject='KDA Flink Snapshot Manager Alert')
+        if pub_response['ResponseMetadata']['HTTPStatusCode'] == 200:
+            message_sent = True
+            logger.info(
+                'Message published to SNS Topic successfully. Message Id: {0}'.format(pub_response['MessageId']))
+    except botocore.exceptions.ClientError as error:
+        if error.response['Error']['Code'] == 'ResourceNotFoundException':
+            logger.warning('The requested SNS Topic was not found')
+        else:
+            print('Error Message: {}'.format(error.response['Error']['Message']))
+    return message_sent
+
+
+def track_snapshot_manager_status(dynamodb, ddb_table_name, primary_partition_key, primary_sort_key, app_name,
+                                  snapshot_manager_run_id, new_snapshot, snapshot_deletion_status):
+    """
+    This function tracks the status of Snapshot Manager
+    :param dynamodb:
+    :param ddb_table_name:
+    :param primary_partition_key:
+    :param primary_sort_key:
+    :param app_name:
+    :param snapshot_manager_run_id:
+    :param new_snapshot:
+    :param snapshot_deletion_status:
+    :return:
+    """
+    item_inserted = False
+    try:
+        # Prepare an item
+        item = {
+            primary_partition_key: {'S': app_name},
+            primary_sort_key: {'N': str(snapshot_manager_run_id)},
+            'new_snapshot_name': {'S': str(new_snapshot['SnapshotName'])},
+            'new_snapshot_create_time': {'S': str(new_snapshot['SnapshotCreationTimestamp'])},
+            'flink_app_version_id': {'S': str(new_snapshot['ApplicationVersionId'])}
+        }
+        if len(snapshot_deletion_status['deleted_snapshots']) > 0:
+            item['snapshots_deleted'] = {'S': str(snapshot_deletion_status['deleted_snapshots'])}
+        if len(snapshot_deletion_status['not_deleted_snapshots']) > 0:
+            item['snapshots_failed_to_be_deleted'] = {'S': str(snapshot_deletion_status['not_deleted_snapshots'])}
+        # Insert the item
+        put_item_response = dynamodb.put_item(TableName=ddb_table_name, Item=item)
+        if put_item_response['ResponseMetadata']['HTTPStatusCode'] == 200:
+            item_inserted = True
+            logger.info('An item inserted successfully')
+    except botocore.exceptions.ClientError as error:
+        if error.response['Error']['Code'] == 'ResourceNotFoundException':
+            logger.warning('The requested DynamoDB table was not found')
+        else:
+            print('Error Message: {}'.format(error.response['Error']['Message']))
+    return item_inserted
